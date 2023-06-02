e61551d2e854fc93a54cf766ad14c9456538c0b9
Jojhan Silva <silvajojhan@gmail.com>
2023-06-02 18:48:13 -0500

    added request for api

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function fetchWeather(type) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=5526d28663647ecbc08db11b4becf0e1")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+      console.log(data)
+      });
+  }
