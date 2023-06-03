e395f18c6225cd8a69d7a4c5c029f74756a678d5
Jojhan Silva <silvajojhan@gmail.com>
2023-06-02 20:52:53 -0500

    openweathermap api added

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,28 @@
+
+
+console.log('hello')
+document.getElementById('cityButton').addEventListener('click', function(event){
+  event.preventDefault()
+  locationString = document.getElementById('city').value;
+  getCoordinate(locationString)
+});
+
+function getCoordinate(locationString) {
+  fetch(`http:api.openweathermap.org/geo/1.0/direct?q=${locationString}&appid=5526d28663647ecbc08db11b4becf0e1`)
+  .then(function (response) {
+    console.log("hi");
+    return response.json();
+    })
+    .then(function(data) {
+      console.log(data)
+      var getLatitude = data.lat
+      var getLongitude = data.lon
+
+      console.log(getLatitude,getLongitude)
+}
+
+)};
+
 function fetchWeather(type) {
     fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=5526d28663647ecbc08db11b4becf0e1")
       .then(function (response) {
@@ -8,4 +33,3 @@ function fetchWeather(type) {
       });
   }
 
-  
