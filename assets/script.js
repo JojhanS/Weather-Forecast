9e998a5aab5bd9e805b981078c71085e3c6c7828
Jojhan Silva <silvajojhan@gmail.com>
2023-06-02 21:05:32 -0500

    fixed fetch request

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,32 @@
-
-
 console.log('hello')
 document.getElementById('cityButton').addEventListener('click', function(event){
   event.preventDefault()
   locationString = document.getElementById('city').value;
   getCoordinate(locationString)
-  });
+});
 
 function getCoordinate(locationString) {
-  fetch(`http:api.openweathermap.org/geo/1.0/direct?q=${locationString}&appid=5526d28663647ecbc08db11b4becf0e1`)
-  .then(function (response) {
-    console.log("hi");
-    return response.json();
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationString}&appid=5526d28663647ecbc08db11b4becf0e1`)
+    .then(function (response) {
+      console.log("hi");
+      return response.json();
     })
     .then(function(data) {
-      console.log(data)
-      var getLatitude = data[0].lat
-      var getLongitude = data[0].lon
+      console.log(data);
+      var getLatitude = data[0].lat;
+      var getLongitude = data[0].lon;
 
-      console.log(getLatitude,getLongitude)
-      fetchWeather(getLatitude,getLongitude)
-},
-      function fetchWeather(getLatitude, getLongitude) {
-        fetch(`http:api.openweathermap.org/data/2.5/forecast?lat=${getLatitude}lon=${getLongitude}&appid=5526d28663647ecbc08db11b4becf0e1`)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-          console.log(data)
-          });
-      }
-      )};
+      console.log(getLatitude, getLongitude);
+      fetchWeather(getLatitude, getLongitude);
+    });
+}
 
-
-
+function fetchWeather(latitude, longitude) {
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=5526d28663647ecbc08db11b4becf0e1`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
