0fb48fddc1893f2f59573772a3952bd5bce8c37e
Jojhan Silva <silvajojhan@gmail.com>
2023-06-06 15:36:08 -0500

    added html containers, populate with api data for weather

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,12 @@ document.getElementById('cityButton').addEventListener('click', function(event){
   event.preventDefault()
   locationString = document.getElementById('city').value;
   getCoordinate(locationString)
+  console.log(locationString)
 });
 
 function getCoordinate(locationString) {
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationString}&appid=5526d28663647ecbc08db11b4becf0e1`)
     .then(function (response) {
-      console.log("hi");
       return response.json();
     })
     .then(function(data) {
@@ -22,11 +22,48 @@ function getCoordinate(locationString) {
 }
 
 function fetchWeather(latitude, longitude) {
-  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=5526d28663647ecbc08db11b4becf0e1`)
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=standard&cnt=5&appid=5526d28663647ecbc08db11b4becf0e1`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      displayForecast(data)
     });
+}
+
+function displayForecast(forecastData){
+// current weather
+  document.getElementById("temp").textContent = "Temperature: " + forecastData.list[0].main.temp;
+  document.getElementById("weatherDesc").textContent =forecastData.list[0].weather[0].description;
+  document.getElementById("windSpeed").textContent = "Wind Speed: " + forecastData.list[0].wind.speed;
+  document.getElementById("humidity").textContent = "Humidity:" + forecastData.list[0].main.humidity;
+  document.getElementById("date").textContent = forecastData.list[0].dt_txt;
+
+// forecast 1
+    document.getElementById("temp1").textContent = forecastData.list[1].main.temp;
+    document.getElementById("weatherDesc1").textContent = forecastData.list[1].weather[0].description;
+    document.getElementById("windSpeed1").textContent = forecastData.list[1].wind.speed;
+    document.getElementById("humidity1").textContent = forecastData.list[1].main.humidity;
+    document.getElementById("date1").textContent = forecastData.list[1].dt_txt;
+  
+//forecast 2
+    document.getElementById("temp2").textContent = forecastData.list[2].main.temp;
+    document.getElementById("weatherDesc2").textContent = forecastData.list[2].weather[0].description;
+    document.getElementById("windSpeed2").textContent = forecastData.list[2].wind.speed;
+    document.getElementById("humidity2").textContent = forecastData.list[2].main.humidity;
+    document.getElementById("date2").textContent = forecastData.list[2].dt_txt;
+//forecast 3
+    document.getElementById("temp3").textContent = forecastData.list[3].main.temp;
+    document.getElementById("weatherDesc3").textContent = forecastData.list[3].weather[0].description;
+    document.getElementById("windSpeed3").textContent = forecastData.list[3].wind.speed;
+    document.getElementById("humidity3").textContent = forecastData.list[3].main.humidity;
+    document.getElementById("date3").textContent = forecastData.list[3].dt_txt;
+  
+//forecast 4
+    document.getElementById("temp4").textContent = forecastData.list[4].main.temp;
+    document.getElementById("weatherDesc4").textContent = forecastData.list[4].weather[0].description;
+    document.getElementById("windSpeed4").textContent = forecastData.list[4].wind.speed;
+    document.getElementById("humidity4").textContent = forecastData.list[4].main.humidity;
+    document.getElementById("date4").textContent = forecastData.list[4].dt_txt;
 }
